6eeacc159b825be074d245ce74f1bed87b7e42df
Danyal Rehan <144559775+DRehan003@users.noreply.github.com>
2026-06-28 01:50:12 -0500

    Add: is-anagram - submission-11

---
+++ Data Structures & Algorithms/is-anagram/submission-11.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        
+        sfreq, tfreq = {}, {}
+        
+        for char in s: 
+            sfreq[char] = sfreq.get(char, 0) + 1
+
+        for char in t:
+            tfreq[char] = tfreq.get(char, 0) + 1
+
+        for key in sfreq.keys():
+            if key not in tfreq or sfreq[key] != tfreq[key]:
+                return False
+
+        return True
