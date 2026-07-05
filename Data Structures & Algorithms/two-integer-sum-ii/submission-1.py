f720af9282d67328d9e160adf004181457200862
Danyal Rehan <144559775+DRehan003@users.noreply.github.com>
2026-07-05 14:56:08 -0500

    Add: two-integer-sum-ii - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-1.py
@@ -0,0 +1,31 @@
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        # non-decreasing order, increasing order
+        # 1-indexed: means that we start index at 1 instead of 0
+        # index1 < index2, index1 != index2
+        # index1 + index2 = target
+        # exactly one valid solution.
+        # solution must be O(1) additional space.
+
+        # create two pointer L and R, create the while loop
+        L = 0
+        R = len(numbers) - 1
+
+        while L < R:
+            # if L + R == target:
+            if numbers[L] + numbers[R] == target:
+                return [L+1, R+1]
+                # return [index1 + 1, index2 + 1]
+
+            #else if L + R > target:
+            elif numbers[L] + numbers[R] > target:
+                R -= 1
+                # R -= 1
+
+            # else if L + R < target:
+            else:
+                L += 1
+                # L += 1
+
+        # loop through the vales and return [index1 + 1, index2 + 1] 
+        
